2fb0f2ad0b5d07af66a61082b099dd5fc5e4bbfd
Felix <felix.p.magnus@gmail.com>
2015-12-23 17:05:08 +0100

    added stuff for giving config file via args

--- wcd.js
+++ wcd.js
@@ -4,14 +4,25 @@ var DOMParser = require('xmldom').DOMParser;
 var xpath = require('xpath');
 var sanitize = require("sanitize-filename");
 var mkdirp = require('mkdirp');
+var program = require('commander');
 
-var useCounterInName = true;
-var maxAmount = 50;
-var counter = 0;
+program
+    .version('0.1')
+    .option('-p, --path [path-to-config-file]', 'Specify the (relative) path to your config file for your webcomic.')
+    .option('-m, --max [number]', 'How many pages you want to download. Default: 0')
+    .option('-c, --counter [number]', 'The initial value of the counter. Default: 0')
+    .option('-n, --name', 'If flag is set, the value of the counter will be used in the file name.')
+    .parse(process.argv);
+
+var useCounterInName = program.name;
+var maxAmount = program.max || 50;
+var counter = program.counter || 0;
+
+var config;
 
 var getInfo = function(url) {
     counter++;
-    if (counter<=maxAmount) {
+    if (counter <= maxAmount) {
         var parser = new DOMParser({
             errorHandler: {
                 warning: null,
@@ -31,27 +42,27 @@ var getInfo = function(url) {
 
                 console.log(counter + " - Started.");
 
-                mkdirp('img/'+config.name, function(err) {
-                    if(err) console.log("Error during folder creation: " + err);
+                mkdirp('img/' + config.name, function(err) {
+                    if (err) console.log("Error during folder creation: " + err);
                 });
 
-                if(imgUrl.indexOf("http:") === -1){
+                if (imgUrl.indexOf("http:") === -1) {
                     imgUrl = "http:" + imgUrl;
                 }
 
                 var fileName;
-                if(useCounterInName){
+                if (useCounterInName) {
                     fileName = "img/" + config.name + "/" + counter + " - " + sanitize(name);
                 } else {
                     fileName = "img/" + config.name + "/" + sanitize(name);
                 }
 
-                downloadPicture(imgUrl, fileName, counter, function(counter, uri){
+                downloadPicture(imgUrl, fileName, counter, function(counter, uri) {
                     console.log(counter + " - Done.");
                 })
 
                 //if nextUrl is a link that does not include the base (e.g. /blub instead of http://comic.com/blub) get the base part from the startUrl and prepend it
-                if(nextUrl.indexOf("http://") === -1 && nextUrl.indexOf("www") === -1){
+                if (nextUrl.indexOf("http://") === -1 && nextUrl.indexOf("www") === -1) {
                     nextUrl = "http://www." + config.startUrl.split("/")[2] + nextUrl;
 
                 }
@@ -75,24 +86,37 @@ var downloadPicture = function(uri, filename, counter, callback) {
     });
 };
 
-var outputInfo = function(config){
-    console.log("WCD - Web Comic Downloader");
-    console.log("--------------------------");
+var outputInfo = function(config) {
     console.log("Config file loaded:");
     console.log("Name: " + config.name);
     console.log("Starting at URL: " + config.startUrl);
-    console.log("XPath for Image: " + config.xpathImg);
-    console.log("XPath for Next: " + config.xpathNext);
-    console.log("XPath for Naming: " + config.xpathNaming);
+    console.log("Starting the counter at: " + counter);
+    console.log("Max Pages to download: " + maxAmount);
     console.log("--------------------------");
-
 }
 
-var start = function(config){
+var handleInputPath = function(path) {
+    if (path[0] !== ".") {
+        if (path[0] === "/") {
+            path = "." + path;
+        } else {
+            path = "./" + path;
+        }
+    }
+
+    if (path[0] === "." && path[1] === ".") {
+        path = "./" + path;
+    }
+    return path;
+}
+
+console.log("--------------------------");
+console.log("WCD - Web Comic Downloader");
+console.log("--------------------------");
+if (program.path) {
+    config = require(handleInputPath(program.path));
     outputInfo(config);
     getInfo(config.startUrl);
-};
-
-//path to your config file goes in here
-var config = require('./config_files/xkcd.json');
-start(config);
+} else {
+    console.log("No config file specified. Use -h for help.");
+}
